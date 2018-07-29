71339b6e856a9ebcdf7495877d3e13e6db500184
Techbinod <35165977+Techbinod@users.noreply.github.com>
2018-07-29 16:59:02 +0545

    dbbase connection established

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+class Database {
+
+	public $conn;
+	public function __construct(){
+		$this->conn = mysqli_connect("localhost","root","","medicine");
+		if($this->conn){
+			echo "Connected";
+		}else{
+			echo "Not connected";
+		}
+	}
+}
+
+
+$obj = new Database;
+
+
+
+
+
+ ?>
